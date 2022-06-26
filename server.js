4ce018837923ef930caad23009d7bb6e303d7814
orlandouchiha425 <valadez425@gmail.com>
2022-06-26 19:04:16 -0400

    finalized Server.js, working on config/models/routes>api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+//morgan provides HTTP errors
 const logger = require('morgan');
 
 const port = 3001;
@@ -9,6 +9,30 @@ const port = 3001;
 
 require('dotenv').config();
 require('./config/databse')
+
+
+app.use(logger('dev'));
+app.use(express.json());
+
+
+// to serve from the production 'build' folder
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Check if token and create req.user
+app.use(require('./config/checkToken'));
+
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'))
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
 
 app.listen(port, () => {
     console.log(`currently running on port ${port}`)
